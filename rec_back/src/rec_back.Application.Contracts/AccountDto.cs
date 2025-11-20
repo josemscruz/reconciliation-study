f9cd0208a479a4e20662f3e99090c63a9119f59b
josemscruz <josemsepcruz@gmail.com>
2025-11-20 22:03:42 +0000

    added optionality to guid

--- rec_back/src/rec_back.Application.Contracts/AccountDto.cs
+++ rec_back/src/rec_back.Application.Contracts/AccountDto.cs
@@ -4,7 +4,7 @@ namespace Reconciliation;
 
 public class AccountDto
 {
-    public Guid Id { get; set; }
+    public Guid? Id { get; set; }
     public string AccountName { get; set; } = string.Empty;
     public string AccountNumber { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
