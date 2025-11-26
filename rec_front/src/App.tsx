bac0f6251206324af15e37f605234ef57ec4db29
josemscruz <josemsepcruz@gmail.com>
2025-11-26 19:55:53 +0000

    finally fixed the issue with the .tsx

--- rec_front/src/App.tsx
+++ rec_front/src/App.tsx
@@ -3,19 +3,25 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
-import "./index.css"
+import "./index.css";
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient();
 
-const App = () => {
-  return (
-    <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </StrictMode>
-  );
-};
+const element = document.getElementById("root");
 
-createRoot(document.getElementById("root")).render(<App />);
+function App() {
+  return (
+    <>
+      <StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </StrictMode>
+    </>
+  );
+}
+
+if (element) {
+  createRoot(element).render(<App />);
+}
